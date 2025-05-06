1e9afb40fadefe4b45fcfd679f4691bdb0ffcea5
Jan Palenik <jan.palenik@icloud.com>
2025-05-06 13:04:09 +0200

    count occurence feature

---
+++ 30-Days_of_JS/arraycount.js
@@ -0,0 +1,13 @@
+// countOccurrences([1, 2, 3, 2, 4, 2], 2) ➞ 3
+// countOccurrences([5, 5, 5, 5], 5) ➞ 4
+// countOccurrences([1, 2, 3], 4) ➞ 0
+
+const countOccurences = (arr, num) => {
+    let count = 0
+    for(let i = 0; i < arr.length; i++ ) {
+        if(arr[i] === num) {
+            count++
+        }
+    }
+    return count 
+}
