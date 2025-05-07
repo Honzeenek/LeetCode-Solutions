dd421b5e61b7249cc153bfaddc07fd14fc21d952
Jan Palenik <jan.palenik@icloud.com>
2025-05-07 08:19:58 +0200

    fix: correct formatting and add missing newline in countOccurences function

--- 30-Days_of_JS/arraycount.js
+++ 30-Days_of_JS/arraycount.js
@@ -3,11 +3,11 @@
 // countOccurrences([1, 2, 3], 4) ➞ 0
 
 const countOccurences = (arr, num) => {
-    let count = 0
-    for(let i = 0; i < arr.length; i++ ) {
-        if(arr[i] === num) {
-            count++
-        }
+  let count = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === num) {
+      count++;
     }
-    return count 
-}
+  }
+  return count;
+};
