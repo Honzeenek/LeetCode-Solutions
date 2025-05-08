249239ca8e31ccbbdee7daeaa9cfa9bec55d9f70
Jan Palenik <jan.palenik@icloud.com>
2025-05-08 13:47:00 +0200

    refactor: simplify filterPositives function using ternary operator

--- 30-Days_of_JS/positives.js
+++ 30-Days_of_JS/positives.js
@@ -1,9 +1,7 @@
 function filterPositives(arr) {
   const positives = [];
   for (let i = 0; i < arr.length; i++) {
-    if (arr[i] > 0) {
-      positives.push(arr[i])
-    }
+    arr[i] > 0 ? positives.push(arr[i]) : null
   }
   return positives;
 }
